db82e0421d08a9bb3bec13141752f31d74ddf43e
Albert Medina <albertmecu@gmail.com>
2026-01-22 22:06:10 +0100

    feat: added generic exception for business rules violation

--- src/main/java/com/videostore/videostore/domain/service/FavouriteDomainService.java
+++ src/main/java/com/videostore/videostore/domain/service/FavouriteDomainService.java
@@ -1,16 +1,18 @@
 package com.videostore.videostore.domain.service;
 
+import com.videostore.videostore.domain.exception.BusinessRuleViolationException;
+
 public class FavouriteDomainService {
 
     public void validateAddFavorite(boolean isAlreadyFavorite) {
         if (isAlreadyFavorite) {
-            throw new RuntimeException("User has already added this movie to favorites");
+            throw new BusinessRuleViolationException("User has already added this movie to favorites");
         }
     }
-    
+
     public void validateRemoveFavorite(boolean isFavorite) {
         if (!isFavorite) {
-            throw new RuntimeException("User cannot remove a movie that is not in favorites");
+            throw new BusinessRuleViolationException("User cannot remove a movie that is not in favorites");
         }
     }
 }
--- src/main/java/com/videostore/videostore/domain/service/ReviewDomainService.java
+++ src/main/java/com/videostore/videostore/domain/service/ReviewDomainService.java
@@ -1,14 +1,16 @@
 package com.videostore.videostore.domain.service;
 
+import com.videostore.videostore.domain.exception.BusinessRuleViolationException;
+
 public class ReviewDomainService {
 
     public void validateReview(boolean hasRentedMovie, boolean hasAlreadyReviewed) {
         if (!hasRentedMovie) {
-            throw new RuntimeException("User cannot review a movie they haven't rented");
+            throw new BusinessRuleViolationException("User cannot review a movie they haven't rented");
         }
 
         if (hasAlreadyReviewed) {
-            throw new RuntimeException("User has already reviewed this movie");
+            throw new BusinessRuleViolationException("User has already reviewed this movie");
         }
     }
 }
