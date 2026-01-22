cc0b685e732f33ac94a5521c731f342eda9816a6
Albert Medina <albertmecu@gmail.com>
2026-01-22 14:18:24 +0100

    initial commit: added project to repository

---
+++ src/main/java/com/videostore/videostore/VideostoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.videostore.videostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostoreApiApplication.class, args);
+	}
+
+}
