41ebf8fb6d8a7578d78b6a76dafe9d0cdf88eca0
Albert Medina <albertmecu@gmail.com>
2026-01-26 14:31:14 +0100

    fix: not allowing 0 movie copies

--- src/main/java/com/videostore/videostore/domain/model/movie/valueobject/NumberOfCopies.java
+++ src/main/java/com/videostore/videostore/domain/model/movie/valueobject/NumberOfCopies.java
@@ -3,7 +3,7 @@ package com.videostore.videostore.domain.model.movie.valueobject;
 public record NumberOfCopies(int value) {
 
     public NumberOfCopies {
-        if (value < 0) {
+        if (value <= 0) {
             throw new IllegalArgumentException("There must be at least one copy");
         }
     }
