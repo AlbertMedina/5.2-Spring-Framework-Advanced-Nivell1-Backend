48d338225e7ec0726cfec539e8d73cf39d650ba1
Albert Medina <albertmecu@gmail.com>
2026-02-02 21:43:35 +0100

    feat: added get favourites by user method in favourite controller

--- src/main/java/com/videostore/videostore/web/controller/favourite/FavouriteController.java
+++ src/main/java/com/videostore/videostore/web/controller/favourite/FavouriteController.java
@@ -4,21 +4,26 @@ import com.videostore.videostore.application.command.favourite.AddFavouriteComma
 import com.videostore.videostore.application.command.favourite.RemoveFavouriteCommand;
 import com.videostore.videostore.application.port.in.favourite.AddFavouriteUseCase;
 import com.videostore.videostore.application.port.in.favourite.RemoveFavouriteUseCase;
+import com.videostore.videostore.application.usecase.favourite.GetFavouritesByUserUseCaseImpl;
 import com.videostore.videostore.domain.model.favourite.Favourite;
 import com.videostore.videostore.web.controller.favourite.dto.request.AddFavouriteRequest;
 import com.videostore.videostore.web.controller.favourite.dto.response.FavouriteResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FavouriteController {
 
     private final AddFavouriteUseCase addFavouriteUseCase;
     private final RemoveFavouriteUseCase removeFavouriteUseCase;
+    private final GetFavouritesByUserUseCaseImpl getFavouritesByUserUseCase;
 
-    public FavouriteController(AddFavouriteUseCase addFavouriteUseCase, RemoveFavouriteUseCase removeFavouriteUseCase) {
+    public FavouriteController(AddFavouriteUseCase addFavouriteUseCase, RemoveFavouriteUseCase removeFavouriteUseCase, GetFavouritesByUserUseCaseImpl getFavouritesByUserUseCase) {
         this.addFavouriteUseCase = addFavouriteUseCase;
         this.removeFavouriteUseCase = removeFavouriteUseCase;
+        this.getFavouritesByUserUseCase = getFavouritesByUserUseCase;
     }
 
     @PostMapping("/favourites")
@@ -35,5 +40,13 @@ public class FavouriteController {
         RemoveFavouriteCommand command = new RemoveFavouriteCommand(userId, movieId);
         removeFavouriteUseCase.execute(command);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/users/{userId}/favourites")
+    public ResponseEntity<List<FavouriteResponse>> getFavouritesByUser(@PathVariable Long userId) {
+        List<FavouriteResponse> response = getFavouritesByUserUseCase.execute(userId)
+                .stream().map(FavouriteResponse::fromDomain).toList();
+
+        return ResponseEntity.ok(response);
     }
 }
