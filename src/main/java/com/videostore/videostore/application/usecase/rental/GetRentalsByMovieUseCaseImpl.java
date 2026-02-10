983528dcc24455f7e14a5e61898bda440c593e9a
Albert Medina <albertmecu@gmail.com>
2026-02-10 21:37:59 +0100

    fix: added validations on get rentals by movie and user use cases

--- src/main/java/com/videostore/videostore/application/usecase/rental/GetRentalsByMovieUseCaseImpl.java
+++ src/main/java/com/videostore/videostore/application/usecase/rental/GetRentalsByMovieUseCaseImpl.java
@@ -1,8 +1,11 @@
 package com.videostore.videostore.application.usecase.rental;
 
 import com.videostore.videostore.application.port.in.rental.GetRentalsByMovieUseCase;
+import com.videostore.videostore.domain.exception.notfound.MovieNotFoundException;
+import com.videostore.videostore.domain.model.movie.Movie;
 import com.videostore.videostore.domain.model.movie.valueobject.MovieId;
 import com.videostore.videostore.domain.model.rental.Rental;
+import com.videostore.videostore.domain.repository.MovieRepository;
 import com.videostore.videostore.domain.repository.RentalRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +16,19 @@ import java.util.List;
 public class GetRentalsByMovieUseCaseImpl implements GetRentalsByMovieUseCase {
 
     private final RentalRepository rentalRepository;
+    private final MovieRepository movieRepository;
 
-    public GetRentalsByMovieUseCaseImpl(RentalRepository rentalRepository) {
+    public GetRentalsByMovieUseCaseImpl(RentalRepository rentalRepository, MovieRepository movieRepository) {
         this.rentalRepository = rentalRepository;
+        this.movieRepository = movieRepository;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Rental> execute(Long movieId) {
-        return rentalRepository.findAllByMovie(new MovieId(movieId));
+        Movie movie = movieRepository.findById(new MovieId(movieId))
+                .orElseThrow(() -> new MovieNotFoundException(movieId));
+
+        return rentalRepository.findAllByMovie(movie.getId());
     }
 }
--- src/main/java/com/videostore/videostore/application/usecase/rental/GetRentalsByUserUseCaseImpl.java
+++ src/main/java/com/videostore/videostore/application/usecase/rental/GetRentalsByUserUseCaseImpl.java
@@ -1,9 +1,16 @@
 package com.videostore.videostore.application.usecase.rental;
 
 import com.videostore.videostore.application.port.in.rental.GetRentalsByUserUseCase;
+import com.videostore.videostore.domain.exception.notfound.MovieNotFoundException;
+import com.videostore.videostore.domain.exception.notfound.UserNotFoundException;
+import com.videostore.videostore.domain.model.movie.Movie;
+import com.videostore.videostore.domain.model.movie.valueobject.MovieId;
 import com.videostore.videostore.domain.model.rental.Rental;
+import com.videostore.videostore.domain.model.user.User;
 import com.videostore.videostore.domain.model.user.valueobject.UserId;
+import com.videostore.videostore.domain.repository.MovieRepository;
 import com.videostore.videostore.domain.repository.RentalRepository;
+import com.videostore.videostore.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,14 +20,19 @@ import java.util.List;
 public class GetRentalsByUserUseCaseImpl implements GetRentalsByUserUseCase {
 
     private final RentalRepository rentalRepository;
+    private final UserRepository userRepository;
 
-    public GetRentalsByUserUseCaseImpl(RentalRepository rentalRepository) {
+    public GetRentalsByUserUseCaseImpl(RentalRepository rentalRepository, UserRepository userRepository) {
         this.rentalRepository = rentalRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Rental> execute(Long userId) {
-        return rentalRepository.findAllByUser(new UserId(userId));
+        User user = userRepository.findById(new UserId(userId))
+                .orElseThrow(() -> new UserNotFoundException(userId));
+
+        return rentalRepository.findAllByUser(user.getId());
     }
 }
